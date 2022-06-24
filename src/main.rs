29cf951a68a7c5aeab0d5a687e0f6b4488785dd5
DrunkenToast <peter5leconte@gmail.com>
2022-06-24 16:12:53 +0200

    Init handshake and led switch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
+use core::time::Duration;
+
 use arduino_hal::{prelude::*, port::{mode::{Output, Input}, Pin}, hal::{port::{PB5, PD0, PD1}, Atmega}, pac::USART0, clock::MHz16, Peripherals, I2c, Delay};
 use arduino_hal::Usart;
-use arduino_hal::Pins;
 use panic_halt as _;
 use embedded_hal::{serial::Read, digital::v2::{OutputPin, PinState}};
 use display_i2c::Lcd;
@@ -12,77 +13,33 @@ mod display_i2c;
 
 #[repr(u8)]
 enum Action {
-    SwitchLed,
-    DisplayMessage,
-    ReadTemperature,
-    ReadHumidity,
+    Hello = 0,
+    SwitchLed = 1,
+    DisplayMessage = 2,
+    ReadTemperature = 3,
+    ReadHumidity = 4,
+    Recv = 5,
 }
 
 impl TryFrom<u8> for Action {
     type Error = ();
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
-        if value <= Action::ReadHumidity as u8 {
-            unsafe {
-                Ok(core::mem::transmute(value))
-            }
-        } else {
-            Err(())
+        match value {
+            0 => Ok(Action::Hello),
+            1 => Ok(Action::SwitchLed),
+            2 => Ok(Action::DisplayMessage),
+            3 => Ok(Action::ReadTemperature),
+            4 => Ok(Action::ReadHumidity),
+            5 => Ok(Action::Recv),
+            _ => Err(()),
         }
     }
 }
+
 type Serial = Usart<USART0, Pin<Input, PD0>, Pin<Output, PD1>>;
 type Led = Pin<Output, PB5>;
 type Display = Lcd<I2c, Delay>;
-
-struct Arduino {
-   serial: Usart<USART0, Pin<Input, PD0>, Pin<Output, PD1>>,
-   led: Pin<Output, PB5>,
-   lcd: Lcd<I2c, Delay>,
-}
-
-impl Arduino {
-    fn new() -> Self {
-        const LCD_ADDR: u8 = 0x27;
-        let dp = arduino_hal::Peripherals::take().unwrap();
-        let pins = arduino_hal::pins!(dp);
-        let led = pins.d13.into_output();
-        let serial = arduino_hal::default_serial!(dp, pins, 115200);
-
-        let i2c = arduino_hal::I2c::new(
-            dp.TWI,
-            pins.a4.into_pull_up_input(),
-            pins.a5.into_pull_up_input(),
-            50_000,
-        );
-
-        let delay = arduino_hal::Delay::new();
-
-        let lcd = Lcd::new(i2c, delay)
-            .address(LCD_ADDR)
-            .cursor_on(false)
-            .rows(2)
-            .init().unwrap();
-
-        Self {
-            led,
-            serial,
-            lcd, 
-        }
-    }
-
-    pub fn write_message(&mut self, message: &str) {
-        self.lcd.clear().unwrap();
-        if message.len() > 16 {
-            self.lcd.write_str(&message[..16]).unwrap();
-            self.lcd.set_cursor(1, 0).unwrap();
-            self.lcd.write_str(&message[16..]).unwrap();
-        }
-        else {
-            self.lcd.write_str(&message).unwrap();
-        }
-    }
-}
 
 const LCD_ADDR: u8 = 0x27;
 
@@ -92,8 +49,8 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let led: Led = pins.d13.into_output();
-    let serial: Serial = arduino_hal::default_serial!(dp, pins, 115200);
+    let mut led: Led = pins.d13.into_output();
+    let mut serial: Serial = arduino_hal::default_serial!(dp, pins, 115200);
 
     let i2c = arduino_hal::I2c::new(
         dp.TWI,
@@ -102,35 +59,48 @@ fn main() -> ! {
         50_000,
     );
 
-    let delay = arduino_hal::Delay::new();
+    let delay_lcd = arduino_hal::Delay::new();
 
-    let lcd: Display = Lcd::new(i2c, delay)
+    let mut display: Display = Lcd::new(i2c, delay_lcd)
         .address(LCD_ADDR)
         .cursor_on(false)
         .rows(2)
         .init().unwrap();
 
-    // Main loop
-    //arduino.write_message("Hello, world! This is a pretty long message");
-    //ufmt::uwrite!(&mut serial, "Hello, world!\r\n").void_unwrap();
+    
+    // Say wait for connection with master
     loop {
-        handle_action(read_action(&mut arduino).unwrap(), &mut arduino);
+        serial.write_byte(Action::Hello as u8);
+        
+        match serial.read() {
+            Ok(b) => {
+                if let Ok(Action::Hello) = Action::try_from(b) {
+                    serial.write_byte(Action::Recv as u8);
+                    break;
+                }
+            }
+            Err(_) => {}
+        }
     }
+    // Main loop
+    loop {
+        handle_action(&mut serial, &mut led, &mut display);
+    }
+
+
+    //ufmt::uwrite!(&mut serial, "Hello, world!\r\n").void_unwrap();
 }
 
-fn read_action(serial: Serial) -> Result<Action, ()> {
-    let b: u8 = serial.read_byte();
-    Action::try_from(b)
-}
-
-fn handle_action(action: Action, serial: Serial, led: Led, display: Display) {
-    match action {
-        Action::SwitchLed => {
+fn handle_action(serial: &mut Serial, led: &mut Led, display: &mut Display) {
+    match Action::try_from(serial.read_byte()) {
+        Ok(Action::Hello) => {},
+        Ok(Action::SwitchLed) => {
             let state: u8 = serial.read_byte();
             if state != 0 {led.set_state(PinState::High).unwrap();}
             else {led.set_state(PinState::Low).unwrap();}
+            serial.write_byte(Action::Recv as u8);
         },
-        Action::DisplayMessage => {
+        Ok(Action::DisplayMessage) => {
             let amt_bytes = serial.read_byte().clamp(0, 32); 
             for _ in 0..amt_bytes {
                 led.toggle();
@@ -142,16 +112,18 @@ fn handle_action(action: Action, serial: Serial, led: Led, display: Display) {
             }
             write_message(core::str::from_utf8(&message).unwrap(), display);
         },
-        Action::ReadTemperature => {
+        Ok(Action::ReadTemperature) => {
+           
+        },
+        Ok(Action::ReadHumidity) => {
             
         },
-        Action::ReadHumidity => {
-            
-        }
+        Ok(Action::Recv) => {},
+        Err(_) => {}
     }
 }
 
-pub fn write_message(message: &str, display: Display) {
+pub fn write_message(message: &str, display: &mut Display) {
     display.clear().unwrap();
     if message.len() > 16 {
         display.write_str(&message[..16]).unwrap();
